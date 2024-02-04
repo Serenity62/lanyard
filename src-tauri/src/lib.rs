2404b684c5745610d8c7f328ece3d5edfbb7895e
Serenity62 <Serenity62@users.noreply.github.com>
2024-02-03 20:32:40 -0500

    Added base logic for creating and loading profiles.

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,115 @@
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::{Read, Write, Result};
+use std::path::Path;
+
+fn read(p: &String) -> Result<String> {
+    // add sanitization to the path argument
+    let path = Path::new(&p);
+    let display = path.display();
+    let mut file = File::options().read(true).write(true)
+        .create(true).open(&path)?;
+        
+    let mut s = String::new();
+    file.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn write(p: &String, s: &String) -> Result<()> {
+    // add sanitization to the path argument
+    let path = Path::new(&p);
+    let display = path.display();
+    let mut file = File::options().write(true).create(true).open(&path)?;
+
+    file.write_all(s.as_bytes())?;
+    Ok(())
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Settings {
+    pub profiles: Vec<Profile>,
+}
+
+impl Settings {
+    pub fn builder(filename: String) -> Settings {
+        let s = match read(&filename) {
+            Err(why) => panic!("Issue opening settings: {}", why),
+            Ok(s) => s,
+        };
+        let settings: Settings = match serde_json::from_str(&s){
+            Err(why) => panic!("Issue processing settings: {}", why),
+            Ok(settings) => settings,
+        };
+        settings
+    }
+
+    pub fn create_profile(&mut self, p: &Profile) -> Result<()> {
+        Ok(())
+    }
+    pub fn delete_profile(&mut self, id:i32) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Profile {
+    pub id: i32,
+    pub name: String,
+    pub location: String,
+}
+
+impl Profile {
+    pub fn get_accounts(&self) -> Result<ProfileAccounts> {
+        let s = read(&self.location)?;
+        let mut accounts: ProfileAccounts = serde_json::from_str(&s)?;
+        Ok(accounts)
+    }
+
+    pub fn add_account(&self, a: &Account) -> Result<()> {
+        let mut accounts = self.get_accounts()?;
+        accounts.accounts.push(&a);
+        let s = serde_json::to_string(&accounts)?;
+        write(&self.location, &s)?;
+        Ok(())
+    }
+
+    pub fn update_account(&self, a: &Account) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn delete_account(&self, id: i32) -> Result<()> {
+        Ok(())
+    }
+    pub fn delete_all(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ProfileAccounts {
+    pub accounts: Vec<Account>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AccountType;
+impl AccountType {
+    pub const BASIC: &'static str = "BASIC";
+    //pub const ACCOUNT: &'static str = "ACCOUNT";
+    //pub const SYSTEM: &'static str = "SYSTEM";
+    //pub const SECRET: &'static str = "SECRET";
+    //pub const RECOVERY: &'static str = "RECOVERY";
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct BasicAccount {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Account {
+    pub id: i32,
+    pub name: String,
+    pub r#type: AccountType,
+    pub account: String,
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,41 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use app::*;
+use serde::{Serialize, Deserialize};
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+
+    tauri::Builder::default()
+        .manage(app::Settings::builder(String::from("settings.json")).into())
+        .invoke_handler(tauri::generate_handerl![get_profile, get_profiles])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn get_profile () -> ProfileAccounts {
+    let p = app::Profile {
+        id: 1,
+        name: String::from("test"),
+        location: String::from("test.json")
+    };
+
+    let as = match p.get_accounts() {
+        Err(why) => panic!("Error when getting accounts: {}", why),
+        Ok(as) => as,
+    };
+    as
+}
+
+#[tauri::command]
+fn get_profiles(settings: tauri::State<Settings>) -> Vec<Profile> {
+    settings.profiles
+}
+
+
+[tauri::command]
+fn new_profile(mut settings: tauri::State<Settings>, p: Profile) -> Result<()> {
+    settings.profiles.push(p);
+    Ok(())
 }
